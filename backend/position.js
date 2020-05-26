050da352637c2682db39fdf53bcabcb0cc66cc8e
Alex Harvey <harvey.alex01+github@gmail.com>
2020-05-26 17:44:37 +1000

    deleted backend node_modules

---
+++ backend/position.js
@@ -0,0 +1,7 @@
+const express = require('express')
+cosnt router = express.Router()
+const ftx = require('ftx-api-ws')
+
+router.post('/', (req, res)=>{
+    
+})
--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [daata, getData] = useState();
+
   return (
     <div className="App">
-
     </div>
   );
 }
