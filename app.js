69e928a6bb6fb478cabcbe77220d5aae01fd4ca4
= <harvey.alex01+github@gmail.com>
2020-06-26 14:44:46 +1000

    enabled restart function

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // start existing positions
-// concurrentPositons.start()
+concurrentPositons.start()
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
