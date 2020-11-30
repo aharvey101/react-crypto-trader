af4796b1f4567e4c08da63090255458751d34ec4
Alexander Harvey <harvey.alex01+github@gmail.com>
2020-11-30 13:30:27 +1100

    added loading to tradeLog route

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -4,19 +4,28 @@ import './nav.css'
 
 function Nav() {
   return (
-
     <nav className="nav">
       <ul className="nav-ul">
         <li className="nav-li">
-          <Link exact='true' to="/" className="nav-link">Order Input</Link>
+          <Link exact="true" to="/" className="nav-link">
+            Order Input
+          </Link>
         </li>
         <li className="nav-li">
-          <Link exact='true' to="/positions" className="nav-link">Positions</Link>
+          <Link exact="trie" to="/overview" className="nav-link">
+            Overview
+          </Link>
         </li>
         <li className="nav-li">
-          <Link exact='true' to="/tradelog" className="nav-link">Trade Log</Link>
+          <Link exact="true" to="/positions" className="nav-link">
+            Positions
+          </Link>
         </li>
-       
+        <li className="nav-li">
+          <Link exact="true" to="/tradelog" className="nav-link">
+            Trade Log
+          </Link>
+        </li>
       </ul>
     </nav>
   )
